ae6d8e360681ab2d4cb0104c064f214dc0226488
Ben Brady <benbrady1992@gmail.com>
2021-01-27 17:52:33 -0500

    Added initial API endpoints

--- RecipeSharingSite/API/API.py
+++ RecipeSharingSite/API/API.py
@@ -1,16 +1,24 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, request
 import json
 
 API = Blueprint('API', __name__)
 
 @API.route('/recipes/')
-def recipes_get():
+def recipes():
     return '/recipes/ API Endpoint'
 
-@API.route('/users/', methods = ['GET'])
+@API.route('/recipes/<recipe_id>', methods=['GET', 'PUT', 'DELETE'])
+def recipes_recipe_id(recipe_id):
+    return '{} /recipes/{} API Endpoint'.format(request.method, recipe_id)
+
+@API.route('/users/')
 def users_get():
     return '/users/ API Endpoint'
 
-@API.route('/users/<user_name>', methods = ['GET'])
-def specific_user_get(user_name):
-    return '/users/' + user_name + ' API Endpoint'
+@API.route('/users/<user_name>', methods=['GET', 'PUT'])
+def users_user_name(user_name):
+    return '{} /users/{} API Endpoint'.format(request.method, user_name)
+
+@API.route('/users/<user_name>/recipes')
+def users_user_name_recipes(user_name):
+    return '/users/{}/recipes API Endpoint'.format(user_name)
